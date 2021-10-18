17ee925d30fe3be25ef613ae86cf696025be2360
Trần Quang Nguyên <tranquangnguyen2811@gmail.com>
2021-10-18 15:32:01 +0700

    .

--- src/pages/guest/Home/Home.js
+++ src/pages/guest/Home/Home.js
@@ -1,7 +1,16 @@
 import { SearchBar } from "../../../components/SearchBar/SearchBar";
 import home from "./home.module.scss";
+import { useClickOutside } from "jnt-hooks";
+import { useRef, useState } from "react";
 
 export const Home = () => {
+  const [isBlue, setIsBlue] = useState(false);
+  const ref = useRef();
+
+  useClickOutside(ref, () => setIsBlue(false));
+
+  console.log("object");
+
   return (
     <div className={home.container}>
       <SearchBar />
