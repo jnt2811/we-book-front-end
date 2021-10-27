5876631149e075066e50733882efd1a2f8ce7c03
Trần Quang Nguyên <tranquangnguyen2811@gmail.com>
2021-10-27 23:40:50 +0700

    .

--- src/pages/guest/ListingView/ListingView.js
+++ src/pages/guest/ListingView/ListingView.js
@@ -1,12 +1,42 @@
 import { useParams } from "react-router-dom";
 import listingView from "./listingView.module.scss";
+import { useState, useEffect } from "react";
+import { requestGet } from "../../../helpers/requestHandler";
+import { apis } from "../../../constants";
+import { Col, Row } from "antd";
 
 export default function ListingView() {
   const { id } = useParams();
 
+  const [listing, setListing] = useState({ gallery: "[]" });
+
+  useEffect(() => {
+    requestGet(apis.LISTING_GUEST + "/" + id).then((result) => {
+      const data = result.data;
+
+      if (data.status) {
+        setListing(data.data[0]);
+      }
+    });
+  }, [id]);
+
   return (
     <div className={listingView["container"]}>
-      <h1>Listing ID: {id}</h1>
+      <div className={listingView["top"]} justify="space-between">
+        <h1 className={listingView["head"]}>{listing.name}</h1>
+
+        <Row justify="space-between">
+          <p className={listingView["sub-head"]}>{listing.address}</p>
+
+          <span>Lưu</span>
+        </Row>
+      </div>
+
+      <div className={listingView["gallery"]}>
+        {JSON.parse(listing.gallery).map((src) => (
+          <img src={src} alt="" className={listingView["gallery-item"]} />
+        ))}
+      </div>
     </div>
   );
 }
