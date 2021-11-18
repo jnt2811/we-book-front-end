d35a4b5d2f3053e33d60bd050019437dd29c510a
Tran Quang Nguyen <tranquangnguyen2811@gmail.com>
2021-11-18 13:24:35 +0700

    .

--- src/pages/guest/Results/Results.js
+++ src/pages/guest/Results/Results.js
@@ -8,33 +8,51 @@ import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { requestPost } from "../../../helpers/requestHandler";
 import { Col, Row, Skeleton } from "antd";
-// import PaginationBar from "./components/PaginationBar/PaginationBar";
+import PaginationBar from "./components/PaginationBar/PaginationBar";
 
 export default function Results() {
   const query = useQuery();
   const history = useHistory();
 
   const destination = query.get(searchKeys.DESTINATION);
-  // const checkin = query.get(searchKeys.CHECKIN);
-  // const checkout = query.get(searchKeys.CHECKOUT);
-  // const guests = query.get(searchKeys.GUESTS);
+  const checkin = query.get(searchKeys.CHECKIN);
+  const checkout = query.get(searchKeys.CHECKOUT);
+  const guests = query.get(searchKeys.GUESTS);
 
+  const [pageSize, setPageSize] = useState(0);
+  const [totalResults, setTotalResults] = useState(0);
+  const [offset, setOffset] = useState(0);
   const [listingList, setListingList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (destination === null || destination === "") history.push(paths.HOME);
+    if (!destination || destination === "") history.push(paths.HOME);
     setIsLoading(true);
-    requestPost(apis.LISTING_GUEST, { destination }).then((result) => {
-      setListingList(result.data.data);
+    requestPost(apis.LISTING_GUEST, {
+      destination,
+      checkin,
+      checkout,
+      guests,
+      offset: offset,
+    }).then((result) => {
+      const { status } = result.data;
+
+      if (status) {
+        const { size, total, listings } = result.data.data;
+
+        setPageSize(size);
+        setTotalResults(total);
+        setListingList(listings);
+      }
+
       setIsLoading(false);
     });
-  }, [destination, history]);
+  }, [checkin, checkout, destination, guests, history, offset]);
 
   return (
     <div className={results["container"]}>
       <div className="list">
-        <p>{listingList.length} chỗ ở</p>
+        <p>{totalResults} chỗ ở</p>
 
         <h1>Chỗ ở tại {destination}</h1>
 
@@ -64,7 +82,14 @@ export default function Results() {
           ))
         )}
 
-        {/* <PaginationBar /> */}
+        {!isLoading && (
+          <PaginationBar
+            offset={offset}
+            pageSize={pageSize}
+            totalResults={totalResults}
+            setOffset={setOffset}
+          />
+        )}
       </div>
 
       <ResultMap />
--- src/pages/guest/Results/components/PaginationBar/PaginationBar.js
+++ src/pages/guest/Results/components/PaginationBar/PaginationBar.js
@@ -1,22 +1,51 @@
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import paginationBar from "./paginationBar.module.scss";
+import cn from "classnames";
+import { useMemo } from "react";
 
-export default function PaginationBar() {
+export default function PaginationBar({
+  offset = 0,
+  pageSize = 1,
+  totalResults = 0,
+  setOffset = () => {},
+}) {
+  const pageArr = useMemo(
+    () =>
+      pageSize > 0
+        ? [
+            ...Array.apply(null, Array(parseInt(totalResults / pageSize))).map(
+              (_, i) => i + 1
+            ),
+          ]
+        : [],
+    [pageSize, totalResults]
+  );
+
   return (
     <div className={paginationBar["container"]}>
-      <button className="btn" onClick={() => console.log("object")} disabled>
+      <button
+        className="btn"
+        onClick={() => setOffset((val) => val - 1)}
+        disabled={pageArr[0] === offset + 1}
+      >
         <LeftOutlined />
       </button>
 
-      <button className="btn active">
-        <span>1</span>
-      </button>
+      {pageArr.map((item) => (
+        <button
+          className={cn("btn", item === offset + 1 && "active")}
+          key={item}
+          onClick={() => setOffset(item - 1)}
+        >
+          <span>{item}</span>
+        </button>
+      ))}
 
-      <button className="btn">
-        <span>2</span>
-      </button>
-
-      <button className="btn">
+      <button
+        className="btn"
+        disabled={pageArr[pageArr.length - 1] === offset + 1}
+        onClick={() => setOffset((val) => val + 1)}
+      >
         <RightOutlined />
       </button>
     </div>
